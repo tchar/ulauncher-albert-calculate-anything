dac6ac4b68d58d331e11a0b88467d71168c4a22b
Tilemachos Charalampous <tilemachos.charalampous@gmail.com>
2021-07-09 14:34:51 +0300

    Update regular expressions

--- calculate_anything/constants.py
+++ calculate_anything/constants.py
@@ -7,15 +7,16 @@ XDG_CACHE = os.getenv('XDG_CACHE_HOME', XDG_FALLBACK)
 CACHE_DIR = os.path.join(XDG_CACHE,  'extension_calculate_anything')
 DATA_FILE = os.path.join(CACHE_DIR, 'data.json')
 
-FILENAME_REGEX = re.compile(r'([a-z]{3})\.json', flags=re.IGNORECASE)
-
+# Replace plus with + and minus with -
 PLUS_MINUS_REPLACE = {'plus': '+', 'minus': '-'}
 PLUS_MINUS_REPLACE = dict((re.escape(k), v) for k, v in PLUS_MINUS_REPLACE.items())
 PLUS_MINUS_REGEX_REPLACE = re.compile("|".join(PLUS_MINUS_REPLACE.keys()), flags=re.IGNORECASE)
 
+# Unit conversion regex match
 UNIT_QUERY_REGEX = re.compile(r'^\s*(.*?)\s+(?:to|in)\s+(.*)$', flags=re.IGNORECASE)
+# Unit conversion when no target unit specified
 UNIT_QUERY_REGEX_DEFAULT = re.compile(r'^\s*(.*)\s*')
-UNIT_REGEX_SPLIT = re.compile(r'[a-z]+')
+UNIT_REGEX_SPLIT = re.compile(r'[^\W_0-9]+')
 
 CURRENCY_QUERY_REGEX = re.compile(r'^\s*(\d+\.?\d*)?\s*(.*)\s+(?:to|in)\s+(.*)$', flags=re.IGNORECASE)
 CURRENCY_QUERY_DEFAULT_REGEX = re.compile(r'^\s*(\d+\.?\d*)?\s*(.*?)\s*(?:to|in)?\s*$', flags=re.IGNORECASE)
@@ -24,7 +25,8 @@ EMPTY_AMOUNT = re.compile(r'^\s*$')
 
 CALCULATOR_ERROR = 1e-10
 CALCULATOR_REGEX_REJECT = re.compile(r'.*(%|\/\/).*')
-CALCULATOR_IMAG_REPLACE = re.compile(r'([^a-z]\s*|\s+|^)i([^a-z0-9]|$)')
+CALCULATOR_IMAG_REPLACE = re.compile(r'([^a-zA-Z]\s*|\s+|^)i([^a-zA-Z0-9]|$)')
+# Replaces imaginary j with 1 j to be computed
 CALCULATOR_IMAG_REGEX_UNIT_REGEX = re.compile(r'([^a-zA-Z0-9]\s*|^\s*)j[^a-zA-Z]{0,1}')
 CALCULATOR_QUERY_REPLACE = {'mod ': '%', 'div ': '//', '^': '**'}
 CALCULATOR_QUERY_REPLACE = dict((re.escape(k), v) for k, v in CALCULATOR_QUERY_REPLACE.items())
@@ -36,7 +38,7 @@ PERCENTAGES_REGEX_CALC_MATCH = re.compile(r'^\s*(.*)\s*(\+|-)\s*(.*)\s*%\s*$')
 
 TIME_QUERY_REGEX = re.compile(r'\s*(now|time)', flags=re.IGNORECASE)
 TIME_QUERY_REGEX_SPLIT = re.compile(r'\s(?:in|at)(?:\s|$)', flags=re.IGNORECASE)
-TIME_SUBQUERY_REGEX = re.compile(r'.*[a-z].*', flags=re.IGNORECASE)
+TIME_SUBQUERY_REGEX = re.compile(r'.*[^\W_0-9].*', flags=re.IGNORECASE|re.UNICODE)
 TIME_SUBQUERY_DIGITS= re.compile(r'\d+\.?\d*')
 TIME_SPLIT_REGEX = re.compile(r'(\+|-)')
 TIME_LOCATION_REPLACE_REGEX = re.compile(r'[\W_]+', flags=re.IGNORECASE|re.UNICODE)
