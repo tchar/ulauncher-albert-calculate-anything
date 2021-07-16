a4383a514c0f6b68f7507678e42a08bde8773cb1
Tilemachos Charalampous <tilemachos.charalampous@gmail.com>
2021-07-16 04:34:22 +0300

    Add tests, typings and format for calculate_anything.utils

--- calculate_anything/utils.py
+++ calculate_anything/utils.py
@@ -1,15 +1,23 @@
+from typing import (
+    Any, Callable, Collection, Container, Dict,
+    Generator, Iterable, List, Optional, Tuple, Type, Union
+)
 import sys
 import re
 from itertools import combinations
 from .exceptions import MissingSimpleevalException
 
-def is_types(*types):
+
+def is_types(*types: List[Type[Any]]) -> bool:
     return lambda value: any(map(lambda t: isinstance(value, t), types))
 
-def is_not_types(*types):
+
+def is_not_types(*types: List[Type[Any]]) -> bool:
     return lambda value: not is_types(*types)(value)
 
-def get_or_default(value, _type, default, allowed_values=None):
+
+def get_or_default(value: Any, _type: Type[Any], default: Any,
+                   allowed_values: Optional[Union[Container, Iterable]] = None) -> Any:
     try:
         value = _type(value)
         if allowed_values and value not in allowed_values:
@@ -18,21 +26,31 @@ def get_or_default(value, _type, default, allowed_values=None):
     except Exception:
         return default
 
+
 class Singleton(type):
     _instances = {}
-    def __call__(cls, *args, **kwargs):
+
+    def __call__(cls, *args: Any, **kwargs: Any) -> Any:
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
-class StupidEval:
-    def __init__(self, *args, **kwargs):
-        self.operators = {}
-    def eval(self, query):
-        try:return int(query)
-        except (ValueError, TypeError): raise MissingSimpleevalException
 
-def partition(l):
+class StupidEval:
+    def __init__(self, *args: Any, **kwargs: Any):
+        self.operators = {}
+
+    def eval(self, query: str) -> int:
+        try:
+            if not isinstance(query, str):
+                raise MissingSimpleevalException
+            return int(query)
+        except (ValueError, TypeError):
+            raise MissingSimpleevalException
+
+
+def partition(l: List[Any]) -> Generator[List[Any], None, None]:
     for i in range(len(l)):
         for comb in combinations(range(1, len(l)), i):
             result = []
@@ -43,37 +61,45 @@ def partition(l):
             result.append(l[prev:])
             yield result
 
-def or_regex(values, include=True):
+
+def or_regex(values: Iterable[str], include: Optional[bool] = True) -> str:
     regex = sorted(values, key=len, reverse=True)
     regex = map(re.escape, regex)
     regex = '|'.join(regex)
-    if include: regex = '(' + regex + ')'
+    if include:
+        regex = '(' + regex + ')'
     return regex
 
-def replace_dict_re_func(replace_dict, sort=True, ignorecase=False):
+
+def replace_dict_re_func(replace_dict: Dict[str, str],
+                         sort: Optional[bool] = True,
+                         ignorecase: Optional[bool] = False) -> Callable[[str], str]:
     if ignorecase:
         replace_dict = {k.lower(): v for k, v in replace_dict.items()}
-        case_f = lambda s: s.lower()
+        def case_f(s): return s.lower()
     else:
-        case_f = lambda s: s
-    
+        def case_f(s): return s
+
     regex = replace_dict
     if sort or sys.version_info[:2] < (3, 7):
         regex = sorted(regex, key=len, reverse=True)
     regex = r'|'.join(map(re.escape, regex))
-    
+
     if ignorecase:
         regex = re.compile(regex, flags=re.IGNORECASE)
     else:
         regex = re.compile(regex)
-    
+
     return lambda s: regex.sub(lambda m: replace_dict[case_f(m.group(0))], s)
 
-def hex_to_rgb(hex):
+
+def hex_to_rgb(hex: str) -> Tuple[int, int, int]:
     r, g, b = tuple(int(hex[i:i + 2], 16) for i in (0, 2, 4))
     return r, g, b
 
-def rgb_to_cmyk(rgb):
+
+# https://ariya.blogspot.com/2008/07/converting-between-hsl-and-hsv.html
+def rgb_to_cmyk(rgb: Collection[int]) -> Tuple[float, float, float]:
     r, g, b = map(lambda v: v / 255., rgb)
     k = 1 - max(r, g, b)
     c = (1 - r - k) / (1 - k) if k != 1 else 0
@@ -82,39 +108,53 @@ def rgb_to_cmyk(rgb):
     c, m, y, k = 100 * c, 100 * m, 100 * y, 100 * k
     return c, m, y, k
 
-def rgb_to_hsv(rgb):
+
+# https://ariya.blogspot.com/2008/07/converting-between-hsl-and-hsv.html
+def rgb_to_hsv(rgb: Collection[int]) -> Tuple[float, float, float]:
     r, g, b = map(lambda v: v / 255., rgb)
     cmax, cmin = max(r, g, b), min(r, g, b)
     delta = cmax - cmin
-    if delta == 0: h = 0
-    elif cmax == r: h = ((g - b) / delta) % 6
-    elif cmax == g: h = ((b - r) / delta) + 2
-    elif cmax == b: h = ((r - g) / delta) + 4
+    if delta == 0:
+        h = 0
+    elif cmax == r:
+        h = ((g - b) / delta) % 6
+    elif cmax == g:
+        h = ((b - r) / delta) + 2
+    elif cmax == b:
+        h = ((r - g) / delta) + 4
     h *= 60
     s = 0 if cmax == 0 else 100 * delta / cmax
     v = 100 * cmax
     return h, s, v
 
+
 # https://ariya.blogspot.com/2008/07/converting-between-hsl-and-hsv.html
-def rgb_to_hsl(rgb):
+def rgb_to_hsl(rgb: Collection[int]) -> Tuple[float, float, float]:
     r, g, b = map(lambda v: v / 255., rgb)
     cmax, cmin = max(r, g, b), min(r, g, b)
     delta = cmax - cmin
-    if delta == 0: h = 0
-    elif cmax == r: h = ((g - b) / delta) % 6
-    elif cmax == g: h = ((b - r) / delta) + 2
-    elif cmax == b: h = ((r - g) / delta) + 4
+    if delta == 0:
+        h = 0
+    elif cmax == r:
+        h = ((g - b) / delta) % 6
+    elif cmax == g:
+        h = ((b - r) / delta) + 2
+    elif cmax == b:
+        h = ((r - g) / delta) + 4
     h *= 60
     l = (cmax + cmin) / 2.
     s = 0 if delta == 0 else 100 * delta / (1 - abs(2 * l - 1))
     l = 100 * l
     return h, s, l
 
-def is_integer(value):
+
+def is_integer(value: Any) -> bool:
     if isinstance(value, float):
         if value.is_integer():
             return True
         return False
     if isinstance(value, complex):
         return value.imag == 0
+    if isinstance(value, bool):
+        return False
     return isinstance(value, int)
---
+++ tests/test_utils.py
@@ -0,0 +1,198 @@
+import pytest
+from collections import OrderedDict
+import calculate_anything.utils as utils
+from calculate_anything.exceptions import MissingSimpleevalException
+
+
+@pytest.mark.parametrize('reverse', [False, True])
+def test_is_types(reverse):
+    if reverse:
+        func = utils.is_not_types
+    else:
+        func = utils.is_types
+
+    for i in range(-1, 10):
+        assert func(int)(i) != reverse
+        assert func(int)(i + 0.2) == reverse
+        assert func(float)(i + 0.1) != reverse
+        assert func(float)(i) == reverse
+        assert func(str)(str(i)) != reverse
+        assert func(str)(i) == reverse
+        assert func(bool)(bool(i)) != reverse
+        assert func(bool)(i) == reverse
+
+    # Multi
+    assert func(int, float, str)(1) != reverse
+    assert func(int, float, str)('test_string') != reverse
+
+    # Assert opposite
+    assert func(str)(2.5) == reverse
+    assert func(int, float, complex)('test_string') == reverse
+
+    assert func(utils.StupidEval)(utils.StupidEval()) != reverse
+
+    with pytest.raises(TypeError):
+        func('object')('object')
+        func('object')(object)
+        func(object)(object)
+        func(1, )(int, object)
+
+
+def test_get_or_default():
+    value, type, default, allowed, expected = (1, int, 0, [], 1)
+    assert utils.get_or_default(value, type, default) == expected
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = (1.1, int, True, [], 1)
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = ('1.1', int, 'True', [], 'True')
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = ('1', int, 'True', [], 1)
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = ('some_text', int, 4, [], 4)
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = (
+        'some text', int, 'some other text', [], 'some other text')
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = (
+        'some text', int, 'other text', [], 'other text')
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = ('0', str, 0, [], '0')
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = ('1.1', float, 0, [], 1.1)
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = ('True', bool, 2, [], True)
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = (1, int, 2, [1], 1)
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = (
+        1, int, 'some value', ['Test', 1], 1)
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = (
+        '1', str, 'some other value', ['1'], '1')
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = (
+        'Test', bool, 'some other value', [2], 'some other value')
+    assert utils.get_or_default(value, type, default, allowed) == expected
+    value, type, default, allowed, expected = (
+        'True', bool, 'some other value', [True], True)
+    assert utils.get_or_default(value, type, default, allowed) == expected
+
+
+def test_singleton():
+    class SingletonClass(metaclass=utils.Singleton):
+        pass
+
+    class SingletonClassWithArgs(SingletonClass):
+        def __init__(self, *args, **kwargs):
+            self.args = args
+            self.kwargs = kwargs
+
+    t1 = SingletonClass()
+    t2 = SingletonClass()
+
+    assert t1 == t2
+    assert id(t1) == id(t2)
+
+    t1 = SingletonClassWithArgs(1, 2, a='a', b='b')
+    t2 = SingletonClassWithArgs(3, 4, c='c', d='d')
+
+    assert t1.args == t2.args
+    assert id(t1.args) == id(t2.args)
+    assert t1.kwargs == t2.kwargs
+    assert id(t1.kwargs) == id(t2.kwargs)
+    assert t1 == t2
+    assert id(t1) == id(t2)
+
+
+def test_stupid_eval():
+    stupid_eval = utils.StupidEval()
+    for i in range(10):
+        assert stupid_eval.eval(str(i)) == i
+
+    with_exception = [
+        '1.1', 'some-text', 'True', '1 + 1',
+        True, 1, 1.2, 1 + 2j, utils.StupidEval,
+        stupid_eval, None
+    ]
+    for v in with_exception:
+        with pytest.raises(MissingSimpleevalException):
+            stupid_eval.eval(v)
+
+
+def test_partition():
+    # TODO: Complete
+    pass
+
+
+def test_or_regex():
+    # TODO: Complete
+    pass
+
+
+def test_replace_dict_re_func():
+    f = utils.replace_dict_re_func
+
+    s = 'abcdefgh'
+
+    # Test normal
+    d = {'abcd': '1234', 'efgh': '5678'}
+    assert f(d)(s) == '12345678'
+
+    d = {'abc': '1', 'ef': '2'}
+    assert f(d)(s) == '1d2gh'
+
+    # Test with substrings
+    d = {'abcd': '1', 'abc': '2'}
+    assert f(d)(s) == '1efgh'
+
+    # Test with ordered dict
+    d = OrderedDict([('abc', '2'), ('abcd', '1')])
+    assert f(d, sort=False)(s) == '2defgh'
+    assert f(d, sort=True)(s) == '1efgh'
+
+    # Test ignore case
+    d = {'ABCD': '1', 'efgh': '2'}
+    assert f(d, ignorecase=False)(s) == 'abcd2'
+    assert f(d, ignorecase=True)(s) == '12'
+
+    # Test unicode
+    s_unicode = 'αβγδεηζθ'
+    d = {'αβγδ': 'ικλμ', 'εηζθ': 'νξοπ'}
+    assert f(d)(s_unicode) == 'ικλμνξοπ'
+
+    # Test exceptions
+    with pytest.raises(TypeError):
+        assert f({'abc': 1})(s)
+        assert f({1: '1'})(s)
+
+
+def test_hex_to_rgb():
+    # TODO: Complete
+    pass
+
+
+def test_rgb_to_cmyk():
+    # TODO: Complete
+    pass
+
+
+def test_rgb_to_hsv():
+    # TODO: Complete
+    pass
+
+
+def test_rgb_to_hsl():
+    # TODO: Complete
+    pass
+
+
+def test_is_integer():
+    # Test some integer numbers
+    integers = [1, 2.0, 1 + 0j, -17.0]
+    for integer in integers:
+        assert utils.is_integer(integer)
+
+    # Test some non integer values
+    not_integers = [1.5, -1.124, 2 + 5j, 'some text', True]
+    for not_integer in not_integers:
+        assert not utils.is_integer(not_integer)
